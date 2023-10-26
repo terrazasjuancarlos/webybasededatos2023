0448b69b97d49b37fe95f90b8fd1b61739fceb33
terrazasjuancarlos <124410875+terrazasjuancarlos@users.noreply.github.com>
2023-10-25 18:59:11 -0700

    Add files via upload

--- Parcial2/ConsultarAlumnos.php
+++ Parcial2/ConsultarAlumnos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>conultar alumnos</title>
+    <title>Document</title>
 </head>
 <body>
     <?php include 'home.php';
@@ -11,5 +11,40 @@
           $sql ="SELECT + FROM alumnos";
           $datos = $conexion->query($sql);   
     ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php if($datos->$num_row->0){?>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <tr>id</tr>
+                                <tr>Nombre</tr>
+                                <tr>Numero de Control</tr>
+                                <tr>Semestre</tr>
+                                <tr>Edad</tr>
+                                <tr>Turno</tr>
+                                <tr>Sexo</tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while($registro = $datos->fetch_assoc()){?>
+                                <tr>
+                                    <td><?php echo $registro["id"]?></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                        </tbody>
+                    </table>
+                <?php }else{?>
+                    <h3>No existen alumnos en la base de datos</h3>
+                <?php}?>        
+            </div>
+        </div>
+    </div>
 </body>
 </html>
