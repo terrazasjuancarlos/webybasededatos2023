c2890b6c028f7f972e8a2dc67f8acb91c8fedbff
terrazasjuancarlos <124410875+terrazasjuancarlos@users.noreply.github.com>
2023-10-25 19:37:29 -0700

    Add files via upload

--- Parcial2/GuardarRegistros.php
+++ Parcial2/GuardarRegistros.php
@@ -1,10 +1,20 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RegistarAlumnos</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include 'conexionn.php';
+
+$nombre = $_POST["nombre"];
+$numero_control = $_POST["nc"];
+$semestre = $_POST["semestre"];
+$edad = $_POST["edad"];
+
+$sql = "INSERT INTO alumnos(nombre, numero_control, semestre, edad, turno, sexo)";
+$sql = "VALUES('".$nombre."', '".$numero_control."', ".$semestre.",".$edad.",'".$turno."',".$sexo.")";
+
+if($conexion->query(sql) === TRUE){
+    header("Location: consultarAlumnos.php");
+    $conexion->close();
+    exit;
+} else {
+    echo "<h2>Ocurrió un error</h2> <p>Error: ".$sql . "<br>" . $conexion-error . "</p>";
+    echo "<h3><a href='consultarAlumnos.php'>Regresar a alumnos</a></h3>";
+
+}
--- Parcial2/RegistrarAlumnos.php
+++ Parcial2/RegistrarAlumnos.php
@@ -1,15 +1,64 @@
-<?php
-    include 'conexion.php'                
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar alumnos</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="jquery-3.7.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 card m-4 p-4">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="">Nombre</label>
+                        <input name="nombre" type="text" class="form-control" placeholder="Teclea el nombre del alumno" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Numero de control:</label>
+                        <input name="nc" type="text" class="form-control" placeholder="Teclea el numero de control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Semestre:</label>
+                        <input name="semestre" type="text" class="form-control" placeholder="Teclea el semestre" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Edad</label>
+                        <input name="edad" type="text" class="form-control" placeholder="Teclea la edad" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Turno</label>
+                        <select name="turno" class="form-control" required>
+                            <option value="">Selecciona el turno</option>
+                            <option value="MATUTINO">Matutino</option>
+                            <option value="VESPERTINO">Vespertino</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="Sexo"></label>
+                        <select name="sexo" class="form-control" required>
+                        <option value="">Selecciona el sexo</option>
+                        <option value="0">FEMENINO</option>
+                        <option value="1">MASCULINO</option>
+                        <option value="2">Prefiero no responder</option>
+                    </select>
+                    </div><br>
+                    <div>
+                        <input type="submit" value="Registrar" class="btn btn-primary">
+                        <a href="consultarAlumnos.php" class="btn btn-danger">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-    $nombre = $_POST["nombre"];
-    $numero_control = $_POST["numero_control"];
-    $semestre = $_POST["semestre"];
-    $edad = $_POST["edad"];
-    $turno = $_POST["turno"];
-    $sexo = $_POST["sexo"];
-
-    $sql="INSET INTG alumnos (nombre,numero_contro, semestre ,edad ,turno,sexo) ";
-    $sql=="VALUEST"       
-    $edad = $_POST["edad"];
-
-?>
+    <footer class="text-center">
+        <hr>
+        CETIS 107 &copy; 2023
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
